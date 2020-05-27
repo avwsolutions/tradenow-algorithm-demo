8ef24d70b5a3eb62bb0f83c6dc176a4f6e21673e
avwsolutions <arnold.van.wijnbergen@gmail.com>
2020-05-27 10:29:45 +0200

    initial commit

---
+++ src/main/java/io/tradenow/algorithm/Secret.java
@@ -0,0 +1,17 @@
+package io.tradenow.algorithm;
+
+/**
+ * Algorithm!
+ */
+public final class Secret {
+    private Secret() {
+    }
+
+    /**
+     * Says the secret to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Enjoy the next signtool Demo !!! This is my secret");
+    }
+}
---
+++ src/test/java/io/tradenow/algorithm/SecretTest.java
@@ -0,0 +1,18 @@
+package io.tradenow.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class SecretTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testSecret() {
+        assertEquals(1, 1);
+    }
+}
